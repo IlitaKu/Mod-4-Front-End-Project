1e8b1388b3309f7e2018ce30e507dffb49d827f3
github username <github email address>
2019-11-29 15:32:36 +0000

    table cards added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import StartPage from "./Container/StartPage";
 import GamePage from "./Container/GamePage";
+import App from "./App.css";
 export default class extends Component {
   render() {
     return (
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,9 +4,12 @@ export default class Card extends Component {
   render() {
     return (
       <div>
-        <div className="image">
-          <h2>{this.props.pokemon.name} </h2>
-          <img alt="Pokemon" src={this.props.pokemon.sprites.front} />
+        <div className="ui four cards">
+          <a className="black card">
+            <div className="image">
+              <img alt="poke" src={this.props.pokemon.sprites.front} />
+            </div>
+          </a>
         </div>
       </div>
     );
