6e5479739dded01086ef4a2c78f38b46d9d81453
github username <github email address>
2019-12-03 13:48:34 +0000

    countdown

--- src/Components/TableComponent.js
+++ src/Components/TableComponent.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import TableCards from "./TableCards";
 
 export default class TableComponent extends Component {
+  state = {
+    timerOn: false,
+    timerStart: 0,
+    timerTime: 0
+  };
   render() {
     return (
       <div>
--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import './App.css';
+import "./App.css";
 import GamePage from "./Container/GamePage";
 import StartPage from "./Container/StartPage";
 import HighscorePage from "./Container/HighscorePage";
 import NoPageFound from "./Container/NoPageFound";
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, Link, BrowserRouter as Router, Switch } from "react-router-dom";
 
 const routing = (
-    <Router>
-        <div>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/start" component={StartPage} />
-                <Route path="/game" component={GamePage} />
-                <Route path="/highscore" component={HighscorePage} />
-                <Route component={NoPageFound} />
-            </Switch>
-        </div>
-    </Router>
-)
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/start" component={StartPage} />
+        <Route path="/game" component={GamePage} />
+        <Route path="/highscore" component={HighscorePage} />
+        <Route component={NoPageFound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 ReactDOM.render(routing, document.getElementById("root"));
 
