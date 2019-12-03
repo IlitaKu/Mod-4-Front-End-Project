252263c9535fee3e3952d8af8a67b8a4f961fe32
github username <github email address>
2019-12-03 11:49:28 +0000

    games controller updated

--- src/Container/GamePage.js
+++ src/Container/GamePage.js
@@ -5,7 +5,7 @@ const PokeAPI = "http://localhost:3002/pokemon";
 
 export default class GamePage extends React.Component {
   state = {
-    pokemons: [],
+    pokemons: []
   };
 
   componentDidMount = () => {
@@ -15,26 +15,17 @@ export default class GamePage extends React.Component {
         this.setState({
           pokemons: pokeData
         })
-      )
+      );
   };
 
   render() {
-    const allShuffledPokemons = this.state.pokemons.sort(() => 0.5 - Math.random())
-    const allCards = allShuffledPokemons.slice(1, 30)
-    const tableCards = allCards.slice(1, 10)
-    const playerCards = allCards.sort(() => 0.5 - Math.random()).slice(1, 4)
+    const allShuffledPokemons = this.state.pokemons.sort(
+      () => 0.5 - Math.random()
+    );
+    const allCards = allShuffledPokemons.slice(1, 30);
+    const tableCards = allCards.slice(1, 10);
+    const playerCards = allCards.sort(() => 0.5 - Math.random()).slice(1, 4);
 
-<<<<<<< HEAD
-    const shuffledPokemons = this.shuffleArray(this.state.pokemons);
-    const randomNinePokemons = shuffledPokemons.slice(1, 10);
-    const randomTwoPokemons = shuffledPokemons.slice(11, 13);
-    const combineTwoArrays = [...randomNinePokemons, ...randomTwoPokemons];
-    let getOneMatch = {};
-    getOneMatch =
-      combineTwoArrays[Math.floor(Math.random(4) * combineTwoArrays.length)];
-    console.log(getOneMatch);
-=======
->>>>>>> 6840909ad70716ba7c96cbf7a78587f427f02a79
     return (
       <div>
         <TableComponent pokemons={tableCards} />
