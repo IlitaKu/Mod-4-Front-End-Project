4378d4b83894595b2b715e2bb03f0a91935785df
thetamicodes <reichold.tamara@gmail.com>
2019-12-04 17:54:22 +0000

    instructions etc

---
+++ src/Components/Instructions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Instructions extends React.Component {
+    render() {
+        return (
+            <div className="instructions">
+                <h2>Instructions</h2>
+                <p>The top cards are the table cards. The bottom three cards are yours. If any of your cards matches any of the table cards, click on your card as quick as possible - you're having 1 second only! Press 'Start Game' to play!</p>
+            </div>
+        )
+    }
+}    
+
+
--- src/Components/UserDetails.js
+++ src/Components/UserDetails.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Avatar from "./Avatar";
 import Score from "./Score";
+import Instructions from "./Instructions";
+
 export default class UserDetails extends Component {
   render() {
     return (
@@ -8,6 +10,7 @@ export default class UserDetails extends Component {
         <Avatar history={this.props.history} />
         <Score score={this.props.score} />
         <h3>Lives left: {this.props.lives}</h3>
+        <Instructions />
       </div>
     );
   }
